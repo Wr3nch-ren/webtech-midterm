2ad4a1c52a8190dd2f0083fe8b18605dce93297b
Thanrada Sonnakhongcharoen <thanrada.son@ku.th>
2023-08-17 19:26:20 +0700

    update posts table

--- database/migrations/2023_08_15_213125_create_posts_table.php
+++ database/migrations/2023_08_15_213125_create_posts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Activity;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -16,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('post_content');
             $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Activity::class);
             $table->timestamps();
         });
     }
