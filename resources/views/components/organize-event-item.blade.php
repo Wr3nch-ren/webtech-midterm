bae1426777eb03b93ac4128646d7fb774bb2c038
thyms-c <thanyamas.c@ku.th>
2023-08-20 03:38:23 +0700

    organize event view

---
+++ resources/views/components/organize-event-item.blade.php
@@ -0,0 +1,31 @@
+@php
+    $tags = ['อาสา', 'วิชาการ', 'เทคโนโลยี'];
+@endphp
+
+<div>
+    <a href="{{ route('organize.home') }}"
+        class="flex items-center bg-white border border-gray-800 rounded-lg shadow hover:bg-gray-100 h-full md:h-64">
+
+        <div class="flex-none border-y-1 w-36 hover:w-48 transition-all duration-200 h-full">
+            <img class="border-r border-gray-800 object-cover object-center rounded-t-lg md:rounded-none md:rounded-l-lg w-full h-full"
+                src="{{ $imgPath }}" alt="">
+        </div>
+
+        <div class="flex justify-around py-6 md:p-0">
+
+            <div class="flex flex-col md:w-2/3 md:max-w-xl p-0">
+                <!-- camp name -->
+                <h5 class="mb-2 text-md font-bold tracking-tight text-gray-900">{{ $eventName }}</h5>
+
+                <div>
+                    <!-- tag -->
+                    @foreach ($tags as $tag)
+                        <x-tag-pill :tag-name="$tag" />
+                    @endforeach
+                </div>
+            </div>
+
+
+        </div>
+    </a>
+</div>
--- resources/views/layouts/subviews/user-sidebar.blade.php
+++ resources/views/layouts/subviews/user-sidebar.blade.php
@@ -7,12 +7,12 @@
         <ul class="text-md space-y-2 font-normal">
 
             <li>
-                    <a href="{{ route('user.profile') }}"
+                <a href="{{ route('user.profile') }}"
                     class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white  dark:hover:bg-gray-700 group hover:text-violet-700 hover:translate-x-1 duration-300 transition-all ease-in-out
                     {{ Route::is('user.profile') ? 'font-semibold' : 'font-medium' }}">
                     <span class="ml-3">ข้อมูลส่วนตัว</span>
                 </a>
-                
+
             </li>
             <li>
                 <a href="{{ route('user.notification') }}"
@@ -36,8 +36,9 @@
                 </a>
             </li>
             <li>
-                <a href="#"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group hover:text-violet-700 hover:translate-x-1 transition-all duration-300 ease-in-out">
+                <a href="{{ route('user.organize') }}"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group hover:text-violet-700 hover:translate-x-1 transition-all duration-300 ease-in-out"
+                    {{ Route::is('user.organize') ? 'font-semibold' : 'font-medium' }}">
                     <span class="ml-3">กิจกรรมที่ดูแล</span>
                 </a>
             </li>
