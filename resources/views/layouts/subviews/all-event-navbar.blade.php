2aa7a2acd8cfca14b37cc510946a1bc5b3c6c568
thyms-c <thanyamas.c@ku.th>
2023-08-20 23:09:21 +0700

    add sort bar in event post page

---
+++ resources/views/layouts/subviews/all-event-navbar.blade.php
@@ -0,0 +1,80 @@
+<!-- sidebar -->
+<aside id="default-sidebar" class=" w-72 pl-28 h-screen transition-transform -translate-x-full sm:translate-x-0"
+    aria-label="Sidebar">
+
+    <div class="h-full py-8 overflow-y-auto flex flex-col items-center">
+
+        <div class="font-normal text-sm pt-4 pl-5">
+            <div class="flex flex-col gap-2">
+                <h3 class="pt-5">หมวดหมู่</h3>
+                <div>
+                    <input type="checkbox" id="coding" name="interest" value="coding" />
+                    <label for="coding" class="px-2">วิชาการ</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="coding" name="interest" value="coding" />
+                    <label for="coding" class="px-2">อาสา</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="coding" name="interest" value="coding" />
+                    <label for="coding" class="px-2">กิจกรรม</label>
+                </div>
+            </div>
+            <div class="flex flex-col gap-2">
+                <h3 class="pt-5">คณะ</h3>
+                <div>
+                    <input type="checkbox" id="coding" name="interest" value="coding" />
+                    <label for="coding" class="px-2">วิทยาศาสตร์</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="coding" name="interest" value="coding" />
+                    <label for="coding" class="px-2">วิศวกรรมศาสตร์</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="coding" name="interest" value="coding" />
+                    <label for="coding" class="px-2">ศึกษาศาสตร์</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="coding" name="interest" value="coding" />
+                    <label for="coding" class="px-2">วิทยาศาสตร์</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="coding" name="interest" value="coding" />
+                    <label for="coding" class="px-2">วิศวกรรมศาสตร์</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="coding" name="interest" value="coding" />
+                    <label for="coding" class="px-2">ศึกษาศาสตร์</label>
+                </div>
+            </div>
+            <div class="flex flex-col gap-2">
+                <h3 class="pt-5">ประเภทค่าย</h3>
+                <div>
+                    <input type="checkbox" id="coding" name="interest" value="coding" />
+                    <label for="coding" class="px-2">ประเภท1</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="coding" name="interest" value="coding" />
+                    <label for="coding" class="px-2">ประเภท2</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="coding" name="interest" value="coding" />
+                    <label for="coding" class="px-2">ประเภท3</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="coding" name="interest" value="coding" />
+                    <label for="coding" class="px-2">ประเภท4</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="coding" name="interest" value="coding" />
+                    <label for="coding" class="px-2">ประเภท5</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="coding" name="interest" value="coding" />
+                    <label for="coding" class="px-2">ประเภท6</label>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</aside>
