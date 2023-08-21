320d46be158e5dbcd00608b1bfced07ecf85daf4
Wr3nch_ren <prayhyper@gmail.com>
2023-08-21 23:14:58 +0700

    Re-Push (forget controller)

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    public function handle($request, Closure $next, $role, $permission = null)
+    {
+        if(!$request->user()->hasRole($role)) {
+            abort(404);
+        }
+
+        if($permission !== null && !$request->user()->can($permission)) {
+            abort(404);
+        }
+
+        return $next($request);
+    }
+}
